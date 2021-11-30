b16492d16c490fe2f336404f53a9a9fcff45f290
guru821 <gururajput605@gmail.com>
2021-11-30 13:07:33 +0530

    Modify the gulpfile.js file and remove the footer logo from get-started twig file and add mixins in scss files

--- web/themes/custom/gamezone/assets/gulpfile.js
+++ web/themes/custom/gamezone/assets/gulpfile.js
@@ -1,9 +1,9 @@
 const { src, dest, watch } = require('gulp');
 const compileSass = require('gulp-sass')(require('sass'));
-const minifyCss = require('gulp-clean-css');
+var sassGlob = require('gulp-sass-glob');
+const cleanCSS = require('gulp-clean-css');
 const sourceMaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
-const cleanCss = require('gulp-clean-css');
 const minifyJs = require('gulp-uglify');
 const concat = require('gulp-concat');
 
@@ -12,25 +12,26 @@ compileSass.compiler = require('node-sass');
 //..........Sass compiler task.............
 const bundleSaas = () => {
     return src('./src/sass/**/*.scss')
-    .pipe(sourceMaps.init())
-    .pipe(compileSass().on('error', compileSass.logError))
-    .pipe(cleanCss())
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(sourceMaps.write())
-    .pipe(dest('./dist/css'));
+        .pipe(sourceMaps.init())
+        .pipe(sassGlob())
+        .pipe(compileSass().on('error', compileSass.logError))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(sourceMaps.write())
+        .pipe(dest('./dist/css'));
 }
 
 //..........Js compiler task.............
 const bundleJs = () => {
     return src('./src/js/*.js')
-    .pipe(sourceMaps.init())
-    .pipe(minifyJs())
-    .pipe(concat('bundle.js'))
-    .pipe(sourceMaps.write())
-    .pipe(dest('./dist/js'));
+        .pipe(sourceMaps.init())
+        .pipe(minifyJs())
+        .pipe(concat('bundle.js'))
+        .pipe(sourceMaps.write())
+        .pipe(dest('./dist/js'));
 }
 
- //..........All compiler task.............  
+//..........All compiler task.............  
 const devWatch = () => {
     watch('./src/sass/**/*.scss', bundleSaas);
     watch('./src/js/*.js', bundleJs);
